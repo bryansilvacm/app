58c70f2f9067f0e1e1d37f530b01a08d2e5969c3
bryansilvacm <brayanaugusto2003@gmail.com>
2024-11-23 19:45:01 -0300

    adiciona index

---
+++ index.js
@@ -0,0 +1,14 @@
+let meta = {
+    value: "ler um livro por mês",
+    checked:true,
+}
+
+//array de objetos
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos por dia",
+        checked: false,
+    }        
+]
+console.log(metas[1].value)
