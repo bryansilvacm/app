92950adfce76fa09b9e31a7c24c473c4c661241e
bryansilvacm <brayanaugusto2003@gmail.com>
2024-11-23 22:12:42 -0300

    add index.js

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 //usamos o prompt para fazer uma caixinha de pergunta para o usuario responder
-const { select } = require('@inquirer/prompts') //informa o node que vamos usar o modulo inquirer, fazendo a requisição do prompts do modulo inquirer
+const { select, input } = require('@inquirer/prompts') //informa o node que vamos usar o modulo inquirer, fazendo a requisição do prompts do modulo inquirer
+let meta = {
+    value:"Tomar 1L de água por dia",
+    checked: false,
+}
+
+let metas = [meta]
+
+const cadastrarMeta = async () => {
+    const meta = await input({message: "Digite a meta:"})
+    if ( meta.length == 0 ){
+        console.log("Atenção a meta não pode ser vazia!")
+        return 
+    } else{
+        metas.push(
+            {value:meta, checked:false}
+        )
+    }
+    
+}
+const listarMetas = async () =>{
+    tamanho = metas.length
+    let count = 0
+    while (count < tamanho){
+        console.log(metas[count])
+        count++
+    }
+
+}
 
 const start = async () =>{ //colocamos o async para poder usar o await depois
 
@@ -12,6 +40,10 @@ const start = async () =>{ //colocamos o async para poder usar o await depois
                     value: "cadastrar"
                 },
                 {
+                    name:"Listar Metas",
+                    value: "listar"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -20,10 +52,10 @@ const start = async () =>{ //colocamos o async para poder usar o await depois
         }) // await faz com o programa espere o usuário selecionar a opção
         switch(opcao){
             case "cadastrar":
-                console.log("vamos cadastrar")
+               await cadastrarMeta()
                 break
             case "listar":
-                console.log("vamos listar")
+                await listarMetas()
                 break
             case "sair":
                 return        
