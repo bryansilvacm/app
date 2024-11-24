5fa1ad9fed78d73ed0dd70cc69c91c0fa24ee313
bryansilvacm <brayanaugusto2003@gmail.com>
2024-11-23 21:19:00 -0300

    add

---
+++ aula.md
@@ -0,0 +1,3 @@
+Módulos/ deoendencias/pacotes - trabalhos de outras pessoas, para usar issp usamos o npm (node package manager) é o gerenciador de pacotes do node
+
+digitamos no terminal "npm install pacote (nome do pacote, ex: inquirer)
--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-let start = () =>{
+//usamos o prompt para fazer uma caixinha de pergunta para o usuario responder
+const { select } = require('@inquirer/prompts') //informa o node que vamos usar o modulo inquirer, fazendo a requisição do prompts do modulo inquirer
+
+const start = async () =>{ //colocamos o async para poder usar o await depois
 
     while(true){
-        let opcao = "cadastrar"
+        const opcao = await select({
+            message:"Menu >",
+            choices:[
+                {
+                    name:"Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+
+            ]
+        }) // await faz com o programa espere o usuário selecionar a opção
         switch(opcao){
             case "cadastrar":
                 console.log("vamos cadastrar")
@@ -11,8 +27,9 @@ let start = () =>{
                 break
             case "sair":
                 return        
-    }
+        }
     }
 }
 
 start()
+
